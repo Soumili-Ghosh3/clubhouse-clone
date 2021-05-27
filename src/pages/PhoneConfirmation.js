3625bf2086088d968fc68b01b00f787bdb3e4317
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-27 18:24:11 +0530

    Added images

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -1,12 +1,26 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import PhoneInput from 'react-phone-number-input'
 import style from '../style/phoneConfirm.module.css'
 
 function PhoneConfirmation() {
     const {value, setValue} = useState();
     return (
-        <div className={style.phoneConfirmConatiner}>
+        <div className={style.phoneConfirmContainer}>
+            <Link exact to="/" className={style.backBtn}>
+                <img src="" alt="" />
+            </Link>
             <h1> Enter your phone </h1>
+
+            <PhoneInput international defaultCountry="IN"
+            value={value} />
+
+            <p>By entering your number, you are agreeing to our {" "}
+                <span>
+                    Terms of Service and Privacy Policy. 
+                </span>
+                Thanks!
+            </p>
         </div>
     )
 }
