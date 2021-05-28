658dc6eb51aacb064fea3fbaf250034881cb1df5
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-28 13:15:21 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import PlanLayout from './pages/layout/PlanLayout';
 import Welcome from './pages/Welcome'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import CodeConfirm from './pages/CodeConfirm'
+import AllowNotification from './pages/AllowNotification';
 
 function App() {
   return (
     <BrowserRouter>
+
+    <Route exact path={[
+      "/",
+      '/invite',
+      '/get_username',
+      '/code_confirm',
+      '/allow_notification',
+    ]}>
+
       <PlanLayout>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/invite" component={PhoneConfirmation} />
-        <Route exact path="/code_confirm" component={CodeConfirm} />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/invite" component={PhoneConfirmation} />
+          <Route exact path="/code_confirm" component={CodeConfirm} />
+          <Route exact path="/allow_notification" component={AllowNotification} />
+        </Switch>
       </PlanLayout>
+
+    </Route>
+
     </BrowserRouter>
   );
 }
