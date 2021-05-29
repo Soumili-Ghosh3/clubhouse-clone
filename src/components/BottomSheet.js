8f20e01464ed72040a8ca6b40a9cad8cc799c557
Soumili-Ghosh3 <1930105@kiit.ac.in>
2021-05-29 22:20:06 +0530

    Changes made

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -2,6 +2,7 @@ import React from 'react'
 import SwipeableBottomSheet from 'react-swipeable-bottom-sheet'
 import style from '../style/bottomSheet.module.css'
 import StartRoom from './bottom_sheets/StartRoom'
+import NewRoom from './bottom_sheets/NewRoom'
 
 function BottomSheet(props) {
     return (
@@ -12,14 +13,32 @@ function BottomSheet(props) {
             props.setItemsVisible(true);
         }}
             fullScreen={props.sheetTitle == 'room detail' ? true : false}>
+
                 <div className={style.BottomSheetContainer}
                 style={{backGroundColor: props.sheetTitle == 'profile' ? 'transparent' : ''}}>
-                    <StartRoom 
-                    setSheetCreateRoom={props.setSheetCreateRoom}
-                    setSheetVisible={(item) => {
-                        props.setSheetVisible(item);
-                        props.setItemsVisible(true);
-                    }}/>
+
+                    {
+                        props.sheetTitle == "new room" ? (
+                            <NewRoom 
+                            cardDetail={props.cardDetail}
+                            setSheetCreateRoom={props.setSheetCreateRoom}
+                            setSheetVisible={(item) => {
+                            props.setSheetVisible(item);
+                            props.setItemsVisible(true);
+                        }}/>
+                        ) : props.sheetTitle == 'start room' ? (
+                            <StartRoom 
+                            cardDetail={props.cardDetail}
+                            setSheetCreateRoom={props.setSheetCreateRoom}
+                            setSheetVisible={(item) => {
+                            props.setSheetVisible(item);
+                            props.setItemsVisible(true);
+                        }}/>
+                        ) :
+                            (
+                            ""
+                        )
+                    }
                 </div>
         </SwipeableBottomSheet>
     );
